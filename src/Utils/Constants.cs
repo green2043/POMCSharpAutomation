b3f595ad469e0d7da2e943b73463e8a0c59a1ebe
Satya <99970061+green2043@users.noreply.github.com>
2025-08-27 21:31:31 -0700

    Create Constants.cs

---
+++ src/Utils/Constants.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace POMCSharpAutomation.Utils
+{
+    public static class Constants
+    {
+        public const string LoginPageTitle = "Account Login";
+        public const string AccountsPageTitle = "My Account";
+        public const string RegisterPageTitle = "Register Account";
+        public const int DefaultWaitTime = 10;
+        // Add more constants as needed
+    }
+}
