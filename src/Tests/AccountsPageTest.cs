7e36227b821df65b279a77bb5bd320a5e2b777cc
Satya <99970061+green2043@users.noreply.github.com>
2025-08-27 21:37:50 -0700

    Create AccountsPageTest.cs

---
+++ src/Tests/AccountsPageTest.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using POMCSharpAutomation.Base;
+using POMCSharpAutomation.Pages;
+using POMCSharpAutomation.Utils;
+
+namespace POMCSharpAutomation.Tests
+{
+    [TestFixture]
+    public class AccountsPageTest : BaseTest
+    {
+        private LoginPage loginPage;
+        private AccountsPage accountsPage;
+
+        [SetUp]
+        public void SetUpTest()
+        {
+            loginPage = new LoginPage(driver);
+            accountsPage = new AccountsPage(driver);
+        }
+
+        [Test]
+        public void VerifyAccountsPageTitle()
+        {
+            loginPage.NavigateTo("https://example.com/login");
+            loginPage.Login("test@example.com", "password");
+            string title = accountsPage.GetAccountsPageTitle();
+            Assert.AreEqual(Constants.HomePageTitle, title);
+        }
+    }
+}
