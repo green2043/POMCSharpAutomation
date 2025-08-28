78550625eecfc19a8b1955ff1d6999ad4574d727
Satya <99970061+green2043@users.noreply.github.com>
2025-08-27 21:28:38 -0700

    Create LoginPageTest.cs

---
+++ src/Tests/LoginPageTest.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using POMCSharpAutomation.Base;
+using POMCSharpAutomation.Pages;
+
+namespace POMCSharpAutomation.Tests
+{
+    [TestFixture]
+    public class LoginPageTest : BaseTest
+    {
+        private LoginPage loginPage;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            loginPage = new LoginPage(driver);
+        }
+
+        [Test]
+        public void VerifyLoginPageTitle()
+        {
+            loginPage.NavigateTo("https://example.com/login");
+            string title = loginPage.GetPageTitle();
+            Assert.AreEqual("Login - My Store", title);
+        }
+
+        [Test]
+        public void VerifyLoginFunctionality()
+        {
+            loginPage.NavigateTo("https://example.com/login");
+            loginPage.Login("test@example.com", "password");
+            // Additional assertions to verify login success can be added here
+        }
+    }
+}
