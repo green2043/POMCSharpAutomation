1d42fae18ecb1a5a921f0bdb6737590051490368
Satya <99970061+green2043@users.noreply.github.com>
2025-08-30 09:12:04 -0700

    Create AppErrors.cs

---
+++ src/Utils/AppErrors.cs
@@ -0,0 +1,13 @@
+namespace POMCSharpAutomation.Utils
+{
+    /// <summary>
+    /// Holds application specific error messages.
+    /// </summary>
+    public static class AppErrors
+    {
+        public const string USERNAME_PASSWORD_INVALID = "Invalid username and password.";
+        public const string ACCOUNT_NOT_FOUND = "Account not found.";
+        public const string PRODUCT_NOT_FOUND = "Product not found.";
+        // Add more error messages as needed
+    }
+}
